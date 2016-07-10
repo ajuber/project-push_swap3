205d2181ab5693fa5092fa9dbb06c30ab27195be
anthony_iceskating@live.fr <anthony_iceskating@live.fr>
2016-07-10 08:00:27 +0200

    b

--- backtrack.c
+++ backtrack.c
@@ -2,15 +2,22 @@
 
 int		backtrack()
 {
+	int i;
+
+	i = 1;
 	if ((liste_triee(t_e *e, nb_op)))
 		return (1);
-	if (check_result_str() != 0)
+	while (i < 12)
 	{
-		create_result_str();
-		if (backtrack() != 0)
-			return (1);
-		del_last_op();
-		del_last_maillon();
+		if (check_result_str(&i) != 0)
+		{
+			create_result_str();
+			if (backtrack() != 0)
+				return (1);
+			del_last_op();
+			del_last_maillon();
+		}
+		i++;
 	}
 	return (0);
 }
